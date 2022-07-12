57bcde8ed5c105df27fe537d1940bc8341eaa7fc
jooyong-boo <qnwndyd159@naver.com>
2022-07-12 17:12:46 +0900

    [Design] 버튼 컬러 변경

--- frontend/src/components/SelectedConditionList.js
+++ frontend/src/components/SelectedConditionList.js
@@ -24,7 +24,7 @@ const CustomForm = styled.form`
     overflow: overlay;
     overflow-x: hidden;
     &::-webkit-scrollbar {
-        width: 8px;
+        width: 6px;
         border-radius: 6px;
     }
     &::-webkit-scrollbar-thumb {
@@ -34,8 +34,12 @@ const CustomForm = styled.form`
 `;
 
 const SelectedConditionList = ({ setTotalAmount, totalAmount, setAmount }) => {
-
-    const notify = (text) => toast.warning(text, { position: "top-center", autoClose: 1000, hideProgressBar: true });
+    const notify = (text) =>
+        toast.warning(text, {
+            position: 'top-center',
+            autoClose: 1000,
+            hideProgressBar: true,
+        });
     const contactRef = useRef(null);
 
     const [selectCondition, setSelectCondition] = useRecoilState(selectConditions);
@@ -47,63 +51,170 @@ const SelectedConditionList = ({ setTotalAmount, totalAmount, setAmount }) => {
     // console.log(selectCondition)
 
     const deleteContidion = (id, plusAmount) => {
-        setSelectCondition(selectCondition.filter(item => item.id !== id ))
-        setTotalAmount(totalAmount + plusAmount)
-        setAmount(0)
-    }
+        setSelectCondition(selectCondition.filter((item) => item.id !== id));
+        setTotalAmount(totalAmount + plusAmount);
+        setAmount(0);
+    };
 
     const onClick = (e) => {
         e.preventDefault();
         if (selectCondition.length === 0) {
             notify('선택한 조건이 없습니다.');
         } else {
-            getFishRecommendData({ personNum, money, area }).then(res => setRecommendList(res.fishRecommendUnions));
+            getFishRecommendData({ personNum, money, area }).then((res) => setRecommendList(res.fishRecommendUnions));
             contactRef.current.scrollIntoView({ behavior: 'smooth' });
         }
-    }
+    };
 
     return (
         <>
             <Card>
-                <Typography sx={{ color: '#575757', padding: '18px 0px 13px 19px', borderBottom: '1px solid #EAEAEA', fontWeight: 'bold'}}>선택한 조건 목록</Typography>
-                    <CustomForm>
+                <Typography
+                    sx={{
+                        color: '#575757',
+                        padding: '18px 0px 13px 19px',
+                        borderBottom: '1px solid #EAEAEA',
+                        fontWeight: 'bold',
+                    }}
+                >
+                    선택한 조건 목록
+                </Typography>
+                <CustomForm>
                     {selectCondition.map((select) => {
-                        {/* console.log(select) */}
+                        {
+                            /* console.log(select) */
+                        }
                         const { id, selectFish, amount, farmStatus, imgUrl } = select;
                         return (
-                            <div style={{ display: 'flex', borderBottom: '1px solid #F6F6F6' }} key={id}>
+                            <div
+                                style={{
+                                    display: 'flex',
+                                    borderBottom: '1px solid #F6F6F6',
+                                }}
+                                key={id}
+                            >
                                 {/* <Img alt="complex" src={image} sx={{ borderRadius: '3px' }} /> */}
                                 <Avatar
                                     alt={selectFish}
                                     src={imgUrl}
-                                    variant= 'square'
-                                    style={{ height: '50px', width: '50px', borderRadius: '3px', margin: '13px' }}
+                                    variant="square"
+                                    style={{
+                                        height: '50px',
+                                        width: '50px',
+                                        borderRadius: '3px',
+                                        margin: '13px',
+                                    }}
                                 />
-                                <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
-                                    <CardContent sx={{ display: 'flex', flexDirection: 'column', padding: '16px 0px' }}>
+                                <div
+                                    style={{
+                                        display: 'flex',
+                                        justifyContent: 'space-between',
+                                        width: '100%',
+                                    }}
+                                >
+                                    <CardContent
+                                        sx={{
+                                            display: 'flex',
+                                            flexDirection: 'column',
+                                            padding: '16px 0px',
+                                        }}
+                                    >
                                         <div style={{ display: 'flex' }}>
-                                            <Typography sx={{ fontSize: 14, color: '#4A4A4A', fontWeight: 'bold' }}>
+                                            <Typography
+                                                sx={{
+                                                    fontSize: 14,
+                                                    color: '#4A4A4A',
+                                                    fontWeight: 'bold',
+                                                }}
+                                            >
                                                 {selectFish}
                                             </Typography>
-                                            <Typography sx={{ fontSize: 13, color: '#A5A5A5', fontWeight: 'medium', marginLeft: 1 }}>
+                                            <Typography
+                                                sx={{
+                                                    fontSize: 13,
+                                                    color: '#A5A5A5',
+                                                    fontWeight: 'medium',
+                                                    marginLeft: 1,
+                                                }}
+                                            >
                                                 ({amount}인)
                                             </Typography>
                                         </div>
-                                        <Typography sx={{ fontSize: 13, color: '#A5A5A5', fontWeight: 'medium' }} color="text.secondary" gutterBottom>
-                                            {farmStatus.join(", ")}
+                                        <Typography
+                                            sx={{
+                                                fontSize: 13,
+                                                color: '#A5A5A5',
+                                                fontWeight: 'medium',
+                                            }}
+                                            color="text.secondary"
+                                            gutterBottom
+                                        >
+                                            {farmStatus.join(', ')}
                                         </Typography>
                                     </CardContent>
                                     <CardActions>
-                                        <Button variant='outlined' sx={{ borderRadius: '1px', color: '#949494', borderColor: '#D8D8D8', fontSize: 12, marginRight: '12px' }} onClick={() => {deleteContidion(id, amount)}}>조건 삭제</Button>
+                                        <Button
+                                            variant="outlined"
+                                            sx={{
+                                                borderRadius: '1px',
+                                                color: '#949494',
+                                                borderColor: '#D8D8D8',
+                                                fontSize: 12,
+                                                marginRight: '12px',
+                                            }}
+                                            onClick={() => {
+                                                deleteContidion(id, amount);
+                                            }}
+                                        >
+                                            조건 삭제
+                                        </Button>
                                         <ToastContainer />
                                     </CardActions>
                                 </div>
                             </div>
-                        )
+                        );
                     })}
-                    </CustomForm>
-                {selectCondition.length > 0 ? <Button variant='contained' disableElevation sx={{ display: 'inline-block', position: 'absolute', backgroundColor: '#0098EE',fontSize: 15 , fontWeight: 900, width: '274px', height: '38px' ,bottom: '9px', left: '10px' }} onClick={onClick} >조합 검색</Button>
-                    : <Button variant='contained' disableElevation sx={{ display: 'inline-block', position: 'absolute', backgroundColor: '#767676',fontSize: 15 , fontWeight: 900, width: '274px', height: '38px' ,bottom: '9px', left: '10px' }} onClick={onClick} >조합 검색</Button>}
+                </CustomForm>
+                {selectCondition.length > 0 ? (
+                    <Button
+                        variant="contained"
+                        disableElevation
+                        sx={{
+                            display: 'inline-block',
+                            position: 'absolute',
+                            backgroundColor: '#0098EE',
+                            fontSize: 15,
+                            fontWeight: 900,
+                            width: '274px',
+                            height: '38px',
+                            bottom: '9px',
+                            left: '10px',
+                        }}
+                        onClick={onClick}
+                    >
+                        조합 검색
+                    </Button>
+                ) : (
+                    <Button
+                        variant="contained"
+                        disableElevation
+                        sx={{
+                            display: 'inline-block',
+                            position: 'absolute',
+                            backgroundColor: '#0098EE',
+                            opacity: 0.3,
+                            fontSize: 15,
+                            fontWeight: 900,
+                            width: '274px',
+                            height: '38px',
+                            bottom: '9px',
+                            left: '10px',
+                        }}
+                        onClick={onClick}
+                    >
+                        조합 검색
+                    </Button>
+                )}
             </Card>
             <SearchResults ref={contactRef} />
         </>
