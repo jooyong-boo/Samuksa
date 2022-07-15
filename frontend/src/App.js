a3ede82117ff1079289598d2b9a130a217c7a061
jooyong-boo <qnwndyd159@naver.com>
2022-07-15 21:06:41 +0900

    [ADD] 라우트 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,11 @@ import './App.css';
 import React, { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Loading from './components/Loading';
-import MainPage from './pages/MainPage';
 import { createTheme, ThemeProvider } from '@mui/material';
+import CalculatorPage from './pages/CalculatorPage';
+import MainPage from './pages/MainPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 
 const theme = createTheme({
     typography: {
@@ -17,6 +20,9 @@ function App() {
             <Suspense fallback={<Loading />}>
                 <Routes>
                     <Route path="/" element={<MainPage />} />
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/register" element={<RegisterPage />} />
+                    <Route path="/calculator" element={<CalculatorPage />} />
                 </Routes>
             </Suspense>
         </ThemeProvider>
