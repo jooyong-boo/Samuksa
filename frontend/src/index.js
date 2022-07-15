4ea0b04013d3341524772cb28ba79dc4b4067e6d
jooyong-boo <qnwndyd159@naver.com>
2022-07-16 03:26:54 +0900

    [ADD] Header 컴포넌트 추가

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,12 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
+import Header from './components/Header';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <RecoilRoot>
             <BrowserRouter>
+                <Header />
                 <App />
             </BrowserRouter>
         </RecoilRoot>
