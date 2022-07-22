2467b41c055008fc100896ccf6d4c149a7ae05bb
jooyong-boo <qnwndyd159@naver.com>
2022-07-22 19:16:13 +0900

    [ADD] 게시판 페이지

---
+++ frontend/src/pages/CommunityPage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import CommunityBoard from '../components/community/CommunityBoard';
+
+const CommunityPage = () => {
+    return <CommunityBoard />;
+};
+
+export default CommunityPage;
