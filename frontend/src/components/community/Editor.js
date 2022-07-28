b0b0f613c852c141c66b4031ae215b538ed193ec
jooyong-boo <qnwndyd159@naver.com>
2022-07-28 16:39:53 +0900

    [ADD] 한국어 언어 설정 추가

--- frontend/src/components/community/Editor.js
+++ frontend/src/components/community/Editor.js
@@ -8,6 +8,7 @@ import { Editor } from '@toast-ui/react-editor';
 import '@toast-ui/editor/dist/toastui-editor.css';
 import 'tui-color-picker/dist/tui-color-picker.css';
 import '@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css';
+import '@toast-ui/editor/dist/i18n/ko-kr';
 import colorSyntax from '@toast-ui/editor-plugin-color-syntax';
 
 const Background = styled.div`
@@ -26,15 +27,20 @@ const Background = styled.div`
 
 const Writing = () => {
     const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
     const onTitleChange = (e) => {
         setTitle(e.target.value);
     };
     const onChange = () => {
         const data = editorRef.current.getInstance().getHTML();
-        console.log(data);
+        setContent(data);
+    };
+    console.log(content);
+    const onSave = ({ title, content }) => {
+        const date = new Date();
     };
     const editorRef = useRef();
-    console.log(editorRef);
+    // console.log(editorRef);
     return (
         <Background>
             <Paper sx={{ height: '95%', width: '95rem', margin: 'auto', marginTop: '30px', padding: '20px' }}>
@@ -46,7 +52,8 @@ const Writing = () => {
                         height: '40px',
                         cursor: 'pointer',
                     }}
-                />
+                />{' '}
+                유저정보 보이는곳
                 <FormControl fullWidth sx={{ m: 1 }} variant="standard">
                     <Input
                         id="title"
@@ -72,11 +79,15 @@ const Writing = () => {
                         ['table', 'image', 'link'],
                     ]}
                     plugins={[colorSyntax]}
-                    // language="ko-KR"
+                    language="ko-KR"
                 />
-                <div style={{ width: '95%', display: 'flex', justifyContent: 'flex-end', marginTop: '1rem' }}>
-                    <Button variant="contained">작성</Button>
-                    <Button variant="outlined">임시저장</Button>
+                <div style={{ width: '99%', display: 'flex', justifyContent: 'flex-end', marginTop: '1rem' }}>
+                    <Button variant="contained" sx={{ width: '7rem', height: '3rem', marginRight: '1rem' }}>
+                        작성
+                    </Button>
+                    <Button variant="outlined" sx={{ width: '7rem', height: '3rem' }}>
+                        임시저장
+                    </Button>
                 </div>
             </Paper>
         </Background>
