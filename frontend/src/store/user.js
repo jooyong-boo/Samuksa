d0b9b783c78e1840588a2a975137cf4a32be3462
jooyong-boo <qnwndyd159@naver.com>
2022-08-05 15:19:41 +0900

    [ADD] 유저정보 selector 추가

--- frontend/src/store/user.js
+++ frontend/src/store/user.js
@@ -1,11 +1,16 @@
 import { atom, selector } from 'recoil';
+import { getUserInfo } from '../api/auth';
 
 export const loginStatusState = atom({
     key: 'loginStatusState',
     default: '',
 });
 
-// export const loginStatusState = atom({
-//     key: 'loginStatusState',
-//     default: '',
-// });
+export const getUserInfoState = selector({
+    key: 'getUserInfoState',
+    get: async ({ get }) => {
+        const response = await getUserInfo();
+        console.log(response);
+        return response;
+    },
+});
