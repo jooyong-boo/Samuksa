38372cbd8761eb2829775f0dc7b3b8dd804d3911
jooyong-boo <qnwndyd159@naver.com>
2022-08-20 01:29:35 +0900

    [EDIT] Typescript 적용

--- frontend/src/components/utils/authHeader.js
+++
@@ -1,9 +0,0 @@
-export const authHeader = () => {
-    const user = JSON.parse(localStorage.getItem('jwtToken'));
-
-    if (user && user.accessToken) {
-        return { Authorization: 'Bearer ' + user };
-    } else {
-        return {};
-    }
-};
