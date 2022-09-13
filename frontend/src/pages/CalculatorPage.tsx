65477718f5019985c22b08ba7882256920a5f8a3
jooyong-boo <qnwndyd159@naver.com>
2022-09-14 00:51:13 +0900

    [Refactor] 코드 리팩토링

--- frontend/src/pages/CalculatorPage.tsx
+++ frontend/src/pages/CalculatorPage.tsx
@@ -1,12 +1,4 @@
-import React from 'react';
-import styled from 'styled-components';
 import Calculator from '../components/calculator/Calculator';
-
-const Background = styled.div`
-    background-color: white;
-    width: 100%;
-    height: 100%;
-`;
 
 const CalculatorPage = () => {
     return <Calculator />;
