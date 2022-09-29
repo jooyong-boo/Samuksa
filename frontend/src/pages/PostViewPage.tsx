6e4101fe3f073f8b548985ba985972c67c0fe5dd
jooyong-boo <qnwndyd159@naver.com>
2022-09-30 00:32:56 +0900

    [EDIT] 파일 위치 이동

--- frontend/src/pages/PostViewPage.tsx
+++
@@ -1,7 +0,0 @@
-import PostViewer from '../components/community/PostViewer';
-
-const PostViewPage = () => {
-    return <PostViewer />;
-};
-
-export default PostViewPage;
---
+++ frontend/src/pages/board/WritingPage.tsx
@@ -0,0 +1,7 @@
+import PostEditor from '../../components/community/PostEditor';
+
+const WritingPage = () => {
+    return <PostEditor />;
+};
+
+export default WritingPage;
