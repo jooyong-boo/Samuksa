909e314a7cc802dd79b893ae2e71ee0c78dbe844
jooyong-boo <qnwndyd159@naver.com>
2022-10-01 00:12:45 +0900

    [ADD] 즐겨찾기 페이지 추가

---
+++ frontend/src/pages/BookmarkPage.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const BookmarkPage = () => {
+    return <Background>즐겨찾기</Background>;
+};
+
+export default BookmarkPage;
+
+const Background = styled.div`
+    background-color: #ebecee;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 60px;
+    overflow: hidden;
+`;
