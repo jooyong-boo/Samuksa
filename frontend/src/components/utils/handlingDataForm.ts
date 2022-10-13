f9980521c21d8d0159b3afaaa935e272a5676ab2
jooyong-boo <qnwndyd159@naver.com>
2022-10-14 01:07:54 +0900

    [EDIT] formData 요청 api 추가 및 일부 조건 삭제

--- frontend/src/components/utils/handlingDataForm.ts
+++ frontend/src/components/utils/handlingDataForm.ts
@@ -1,6 +1,8 @@
+import { changeUserImage } from '../../api/auth';
+
 const handlingDataForm = async (dataURI: any) => {
     const byteString = atob(dataURI.split(',')[1]);
-
+    console.log(dataURI);
     const ab = new ArrayBuffer(byteString.length);
     const ia = new Uint8Array(ab);
     for (let i = 0; i < byteString.length; i++) {
@@ -14,21 +16,17 @@ const handlingDataForm = async (dataURI: any) => {
     // 위 과정을 통해 만든 image폼을 FormData에 넣어줍니다.
     // 서버에서는 이미지를 받을 때, FormData가 아니면 받지 않도록 세팅해야합니다.
     const formData: FormData = new FormData();
-    formData.append('representative_avatar', file);
-    console.log(blob, file);
+    formData.append('image', file);
 
-    // 필요시 더 추가합니다.
-    formData.append('name', 'nkh');
-    formData.append('email', 'noh5524@gmail.com');
     for (let entry of formData.entries()) {
         console.log(entry);
     }
-    // try {
-    //     const changeAvatar = await apis.auth.changeUserAccount(formData);
-    //     alert(changeAvatar.status);
-    // } catch (error) {
-    //     alert(error.response.data.errors);
-    // }
+    try {
+        const changeAvatar = await changeUserImage(formData);
+        console.log(changeAvatar);
+    } catch (error) {
+        console.log(error.response.data.errors);
+    }
 };
 
 export default handlingDataForm;
