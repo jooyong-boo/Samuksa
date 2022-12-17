7d790672565e5d322ae186db29d80b0a21e28aaa
jooyong-boo <qnwndyd159@naver.com>
2022-12-17 09:23:00 +0900

    [ADD] 이미지 변환 로직

--- frontend/src/components/utils/handlingDataForm.ts
+++ frontend/src/components/utils/handlingDataForm.ts
@@ -2,7 +2,6 @@ import { changeUserImage } from '../../api/auth';
 
 const handlingDataForm = async (dataURI: any) => {
     const byteString = atob(dataURI.split(',')[1]);
-    console.log(byteString);
     const ab = new ArrayBuffer(byteString.length);
     const ia = new Uint8Array(ab);
     for (let i = 0; i < byteString.length; i++) {
@@ -11,17 +10,13 @@ const handlingDataForm = async (dataURI: any) => {
     const blob = new Blob([ia], {
         type: 'image/jpeg',
     });
-    const file = new File([blob], 'image.jpg');
-    console.log(blob, file);
+    const file = new File([blob], 'image.jpg', { type: 'image/jpeg' });
     const formData: FormData = new FormData();
     formData.append('image', file);
 
-    for (let entry of formData.entries()) {
-        console.log(entry);
-    }
     try {
         const changeAvatar = await changeUserImage(formData);
-        console.log(changeAvatar);
+        return changeAvatar;
     } catch (error: any) {
         console.log(error.response.data.errors);
     }
