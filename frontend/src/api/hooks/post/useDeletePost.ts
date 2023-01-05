607faac62df5e0a38847b9dc6cbdc71f6229ee46
jooyong-boo <qnwndyd159@naver.com>
2023-01-06 00:54:51 +0900

    add: 글 삭제 hooks

--- frontend/src/api/hooks/post/useDeletePost.ts
+++ frontend/src/api/hooks/post/useDeletePost.ts
@@ -1,14 +1,16 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { deletePost } from 'api/post';
 import { useNavigate } from 'react-router-dom';
+import { notifySuccess } from 'utils/notify';
 
-export const useDeletePost = (idx: string) => {
+export const useDeletePost = (idx: string | number) => {
     const queryClient = useQueryClient();
     const navigate = useNavigate();
     const { mutate } = useMutation(() => deletePost(idx), {
         onSuccess: () => {
             queryClient.invalidateQueries({ queryKey: ['post'] }).then(() => {
                 navigate(-1);
+                notifySuccess('삭제 완료');
             });
         },
     });
