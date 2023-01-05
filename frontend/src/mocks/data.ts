59982af3c490b64d09e32fc65b7bc3a7475cf09b
jooyong-boo <qnwndyd159@naver.com>
2023-01-06 00:57:44 +0900

    add: 댓글 mock data 추가

--- frontend/src/mocks/data.ts
+++ frontend/src/mocks/data.ts
@@ -1,7 +1,7 @@
 export const post = {
     content: [
         {
-            idx: 0,
+            idx: 1,
             profileImage: 'http://localhost:8081/user/images/37c025f0-32bc-4f44-be73-5de992acb765.jpg',
             nickName: '삼먹사',
             createdAt: '2022-12-27 22:56:27.0',
@@ -11,6 +11,50 @@ export const post = {
             viewCount: 123,
             commentCount: 3,
             recommendCount: 5,
+        },
+    ],
+};
+
+export const comments = {
+    totalCommentCount: 2,
+    data: [
+        {
+            idx: 1,
+            avatarUrl: 'http://localhost:8081/user/images/37c025f0-32bc-4f44-be73-5de992acb765.jpg',
+            nickName: '삼먹사',
+            content: '첫번째 댓글',
+            createdAt: '2022-12-27 22:56:27.0',
+            modifiedAt: '2022-12-27 22:56:27.0',
+            command: [
+                {
+                    idx: 1,
+                    avatarUrl: 'http://localhost:8081/user/images/37c025f0-32bc-4f44-be73-5de992acb765.jpg',
+                    nickName: '삼먹사2',
+                    receiverNickName: '삼먹사',
+                    content: '첫번째 댓글',
+                    createdAt: '2022-12-27 22:56:27.0',
+                    modifiedAt: '2022-12-27 22:56:27.0',
+                },
+            ],
+        },
+        {
+            idx: 1,
+            avatarUrl: 'http://localhost:8081/user/images/37c025f0-32bc-4f44-be73-5de992acb765.jpg',
+            nickName: '사먹사',
+            content: '두번째 댓글',
+            createdAt: '2022-12-27 22:56:27.0',
+            modifiedAt: '2022-12-27 22:56:27.0',
+            command: [
+                {
+                    idx: 1,
+                    avatarUrl: 'http://localhost:8081/user/images/37c025f0-32bc-4f44-be73-5de992acb765.jpg',
+                    nickName: '삼먹사2',
+                    receiverNickName: '삼먹사',
+                    content: '첫번째 댓글',
+                    createdAt: '2022-12-27 22:56:27.0',
+                    modifiedAt: '2022-12-27 22:56:27.0',
+                },
+            ],
         },
     ],
 };
