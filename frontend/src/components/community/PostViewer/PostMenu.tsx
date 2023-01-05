af67a6aaeadf864ad89b0395a2fc1c11b09d3bd3
jooyong-boo <qnwndyd159@naver.com>
2023-01-06 00:56:58 +0900

    add: 게시글 메뉴 컴포넌트

---
+++ frontend/src/components/community/PostViewer/PostMenu.tsx
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import MenuIcon from '@mui/icons-material/Menu';
+import { useParams } from 'react-router-dom';
+
+interface IProps {
+    delete: (idx: number | string) => void;
+}
+
+export default function PostMenu({ ...props }: IProps) {
+    const { id } = useParams<{ id: string }>();
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    return (
+        <div>
+            <Button
+                id="basic-button"
+                aria-controls={open ? 'post-menu' : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? 'true' : undefined}
+                onClick={handleClick}
+            >
+                <MenuIcon sx={{ color: '#0098ee' }} />
+            </Button>
+            <Menu
+                id="post-menu"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                MenuListProps={{
+                    'aria-labelledby': 'post-button',
+                }}
+            >
+                <MenuItem
+                    onClick={() => {
+                        handleClose();
+                    }}
+                >
+                    수정하기
+                </MenuItem>
+                <MenuItem
+                    onClick={() => {
+                        handleClose();
+                        props.delete(id!);
+                    }}
+                >
+                    삭제하기
+                </MenuItem>
+            </Menu>
+        </div>
+    );
+}
