1240ef70613092f22243ad29e762b3466864b2da
jooyong-boo <qnwndyd159@naver.com>
2023-01-08 20:33:26 +0900

    edit: 댓글 등록 msw 테스트

--- frontend/src/api/hooks/post/useCreateComment.ts
+++ frontend/src/api/hooks/post/useCreateComment.ts
@@ -2,13 +2,12 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { createComment } from 'api/post';
 import { notifySuccess } from 'utils/notify';
 
-export const useCreateComment = (commentIdx: number, comment: string, titleIdx: number) => {
+export const useCreateComment = (commentIdx: number, comment: string, titleIdx: number | string) => {
     const queryClient = useQueryClient();
     const { mutate } = useMutation(() => createComment(commentIdx, comment, titleIdx), {
-        onSuccess: () => {
-            queryClient.invalidateQueries({ queryKey: ['post'] }).then(() => {
-                notifySuccess('수정 완료');
-            });
+        onSuccess: (res) => {
+            queryClient.setQueryData(['comment'], () => res.data);
+            notifySuccess('댓글 등록 완료');
         },
     });
     return { mutate };
