176acd8a0ffa20c1b3f768343eadff9b35fc4feb
jooyong-boo <qnwndyd159@naver.com>
2023-01-10 22:28:01 +0900

    add: 댓글 태그 닉네임 표시

--- frontend/src/components/community/PostViewer/Reply.tsx
+++ frontend/src/components/community/PostViewer/Reply.tsx
@@ -23,27 +23,45 @@ const Reply = ({ command }: IProps) => {
             {command.map((item: ReplyProps, i: number) => {
                 const { idx, avatarUrl, nickName, receiverNickName, content, createdAt, modifiedAt } = item;
                 return (
-                    <div
-                        style={{
-                            marginLeft: '2rem',
-                            paddingLeft: '2rem',
-                            borderLeft: '2px solid #A7A7A7',
-                            marginBottom: '1rem',
-                        }}
-                        key={idx}
-                    >
+                    <Container key={idx}>
                         <UserInfo profileImage={avatarUrl} nickName={nickName} createdAt={createdAt} />
+                        {receiverNickName ? (
+                            <ReceiverInfoBox>
+                                <ReceiverInfoText>@{receiverNickName}</ReceiverInfoText>
+                            </ReceiverInfoBox>
+                        ) : null}
                         <CommentText>{content}</CommentText>
-                    </div>
+                    </Container>
                 );
             })}
         </>
     );
 };
 
+const Container = styled.div`
+    margin-left: 2rem;
+    padding-left: 2rem;
+    border-left: 2px solid #a7a7a7;
+    margin-bottom: 1rem;
+`;
+
+const ReceiverInfoBox = styled.div`
+    display: inline-block;
+    background-color: #eaecef;
+    padding: 0.3rem 0.5rem;
+    border-radius: 5px;
+`;
+
+const ReceiverInfoText = styled.span`
+    color: #7863d2;
+    font-weight: 700;
+    font-size: 0.97rem;
+`;
+
 const CommentText = styled(Typography)`
     color: rgb(55 65 81);
     margin-bottom: 1rem;
+    font-size: 1.125rem;
 `;
 
 export default Reply;
