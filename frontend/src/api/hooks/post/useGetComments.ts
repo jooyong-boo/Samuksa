f1c49e57e6ce3f1780880f89e8663337228cd98c
jooyong-boo <qnwndyd159@naver.com>
2023-01-13 21:12:16 +0900

    edit: 리턴 데이터 수정

--- frontend/src/api/hooks/post/useGetComments.ts
+++ frontend/src/api/hooks/post/useGetComments.ts
@@ -1,9 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 import { getPostComment } from 'api/post';
-import { useState } from 'react';
 
 const useGetComments = (boardTitleIdx: number | string, page: number, size: number) => {
-    const [total, setTotal] = useState(0);
     const {
         isLoading,
         isError,
@@ -14,13 +12,10 @@ const useGetComments = (boardTitleIdx: number | string, page: number, size: numb
         queryKey: ['comment'],
         queryFn: () => getPostComment(boardTitleIdx, page, size).then((res) => res.data),
         select: (data) => {
-            return data.data;
-        },
-        onSuccess: (data) => {
-            setTotal(data.data.totalCommentCount);
+            return data;
         },
     });
-    return [data, total, isLoading, refetch];
+    return [data, isLoading, refetch];
 };
 
 export default useGetComments;
