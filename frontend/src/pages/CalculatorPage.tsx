15418c5889ad458f3fde2fde6db57a16d561be24
jooyong-boo <qnwndyd159@naver.com>
2023-02-19 22:33:46 +0900

    edit: 컴포넌트 리팩토링

--- frontend/src/pages/CalculatorPage.tsx
+++ frontend/src/pages/CalculatorPage.tsx
@@ -1,7 +1,28 @@
-import Calculator from '../components/calculator/Calculator';
+import BackgroundWave from 'components/BackgroundWave';
+import DetailedSearchConditions from 'components/calculator/DetailedSearchConditions';
+import SearchConditions from 'components/calculator/SearchConditions';
+import styled from 'styled-components';
 
 const CalculatorPage = () => {
-    return <Calculator />;
+    return (
+        <Background>
+            <SearchConditions />
+            <DetailedSearchConditions />
+            <BackgroundWave />
+        </Background>
+    );
 };
+
+const Background = styled.div`
+    background-color: #ebecee;
+    width: 100vw;
+    height: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    padding-top: 104px;
+    overflow: hidden;
+`;
 
 export default CalculatorPage;
