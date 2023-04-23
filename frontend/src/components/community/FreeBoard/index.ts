3acbcfa19000946ea37b20718ee2f9d16d1f550a
jooyong-boo <qnwndyd159@naver.com>
2023-04-23 19:14:56 +0900

    feat: index.ts 파일 생성

---
+++ frontend/src/components/community/FreeBoard/index.ts
@@ -0,0 +1,5 @@
+export { default as MobileBoard } from './MobileBoard';
+export { default as SearchMenu } from './SearchMenu';
+export { default as SortMenu } from './SortMenu';
+export { default as TableBoard } from './TableBoard';
+export { default as WriteBtn } from './WriteBtn';
