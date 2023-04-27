3737177ce48a009bb91239211c993892b3a0141b
jooyong-boo <qnwndyd159@naver.com>
2023-04-27 23:51:51 +0900

    edit: 주석된 코드 미사용으로 제거

--- frontend/src/stories/TextField.stories.ts
+++ frontend/src/stories/TextField.stories.ts
@@ -6,9 +6,6 @@ const meta: Meta<typeof TextField> = {
     title: 'Example/TextField',
     component: TextField,
     tags: ['autodocs'],
-    // argTypes: {
-    //   backgroundColor: { control: 'color' },
-    // },
 };
 
 export default meta;
